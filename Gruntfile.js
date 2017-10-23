a4f3460dd505c88739ac9ced0f37e4c3e28cc91e
sunmee.kim <skt.sunmee.kim@sk.com>
2017-10-23 17:32:47 +0900

    첫번째 커밋

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,16 +20,17 @@ module.exports = function (grunt) {
         CSS_NAME2 : 'btv_wikipedia',
         CSS_NAME3 : 'btv_fortune',
         CSS_NAME4 : 'btv_traffic',
+        CSS_NAME5 : 'btv_help',
+        CSS_NAME6 : 'btv_order',
+        CSS_NAME7 : 'btv_shopping',
+        CSS_NAME8 : 'btv_sports',
+        CSS_NAME9 : 'btv_dictionary',
         CSS_STATIC_NAME : 'btv',
         // STATIC_DIR
         PUBLIC_DIR: './markup/dist/assets',
         BASE_DIR  : "",
         HTML_DIR  : "./markup/html",
-        BUILD_DIR : "./markup/dist/html_build",
-        // IMG_DIR : './markup/dist/assets/images',
-        //prd 주소 https://static.hiddenx.co.kr/hidden/img
-        //dev프록시 할 때 ../../../../static/assets/images
-        //local ../../../../dist/assets/images
+        BUILD_DIR : "./markup/dist/html",
         // Metadata.
         pkg: grunt.file.readJSON("package.json"),
         banner: '/*!  <%= pkg.title || pkg.name %> - v<%= pkg.version %> ' + grunt.util.linefeed + '<%= pkg.author.name %>' + grunt.util.linefeed + ' */' + grunt.util.linefeed,
@@ -42,11 +43,6 @@ module.exports = function (grunt) {
             public: [
                 '<%= PUBLIC_DIR %>/images',
                 '<%= PUBLIC_DIR %>/css/**/*'
-                // '<%= PUBLIC_DIR %>/css/pc/*',
-                // '<%= PUBLIC_DIR %>/css/mobile/*'
-                // '!<%= PUBLIC_DIR %>/css/themes/**',
-                // '!<%= PUBLIC_DIR %>/css/{bootstrap-tagsinput,plugins,styles,font}.css',
-                // '!<%= PUBLIC_DIR %>/css/font/**'
             ],
             html_build: [
                 '<%= BUILD_DIR %>'
@@ -86,62 +82,6 @@ module.exports = function (grunt) {
             }
         },
 
-        // sprite: {
-        //     spr_img: {
-        //         src: ['./markup/img/spr_img/*.png'],
-        //         dest: '<%= PUBLIC_DIR %>/images/spr_img.png',
-        //         imgPath: '../images/spr_img.png',
-        //         destCss: './markup/sass/sprites/_spr_img.scss',
-        //         padding: 4,
-        //         cssSpritesheetName: 'spr_img'
-        //     },
-
-        //     spr_img_2x: {
-        //         src: ['./markup/img/spr_img/2x/*.png'],
-        //         dest: '<%= PUBLIC_DIR %>/images/spr_img_2x.png',
-        //         imgPath: '../images/spr_img_2x.png',
-        //         destCss: './markup/sass/sprites/_spr_img_2x.scss',
-        //         padding: 4,
-        //         cssSpritesheetName: 'spr_img_2x'
-        //     },
-
-        //     spr_service: {
-        //         src: ['./markup/img/spr_service/*.png'],
-        //         dest: '<%= PUBLIC_DIR %>/images/spr_service.png',
-        //         imgPath: '../images/spr_service.png',
-        //         destCss: './markup/sass/sprites/_spr_service.scss',
-        //         padding: 4,
-        //         cssSpritesheetName: 'spr_service'
-        //     },
-
-        //     spr_service_2x: {
-        //         src: ['./markup/img/spr_service/2x/*.png'],
-        //         dest: '<%= PUBLIC_DIR %>/images/spr_service_2x.png',
-        //         imgPath: '../images/spr_service_2x.png',
-        //         destCss: './markup/sass/sprites/_spr_service_2x.scss',
-        //         padding: 4,
-        //         cssSpritesheetName: 'spr_service_2x'
-        //     }
-
-        //     // spr_template: {
-        //     //     src: ['./develop/markup/img/spr_template/*.png'],
-        //     //     dest: '<%= PUBLIC_DIR %>/images/spr_template.png',
-        //     //     imgPath: '<%= IMG_DIR %>/spr_template.png',
-        //     //     destCss: './develop/markup/sass/sprites/_spr_template.scss',
-        //     //     padding: 4,
-        //     //     cssSpritesheetName: 'spr_template'
-        //     // },
-        //     //
-        //     // spr_template_2x: {
-        //     //     src: ['./develop/markup/img/spr_template/2x/*.png'],
-        //     //     dest: '<%= PUBLIC_DIR %>/images/spr_template_2x.png',
-        //     //     imgPath: '<%= IMG_DIR %>/spr_template_2x.png',
-        //     //     destCss: './develop/markup/sass/sprites/_spr_template_2x.scss',
-        //     //     padding: 4,
-        //     //     cssSpritesheetName: 'spr_template_2x'
-        //     // }
-        // },
-
         sass: {
             options: {
                 outputStyle: 'compressed',
@@ -154,10 +94,12 @@ module.exports = function (grunt) {
                     '<%= PUBLIC_DIR %>/css/<%= CSS_NAME2 %>.css': './markup/sass/<%= CSS_NAME2 %>.scss',
                     '<%= PUBLIC_DIR %>/css/<%= CSS_NAME3 %>.css': './markup/sass/<%= CSS_NAME3 %>.scss',
                     '<%= PUBLIC_DIR %>/css/<%= CSS_NAME4 %>.css': './markup/sass/<%= CSS_NAME4 %>.scss',
+                    '<%= PUBLIC_DIR %>/css/<%= CSS_NAME5 %>.css': './markup/sass/<%= CSS_NAME5 %>.scss',
+                    '<%= PUBLIC_DIR %>/css/<%= CSS_NAME6 %>.css': './markup/sass/<%= CSS_NAME6 %>.scss',
+                    '<%= PUBLIC_DIR %>/css/<%= CSS_NAME7 %>.css': './markup/sass/<%= CSS_NAME7 %>.scss',
+                    '<%= PUBLIC_DIR %>/css/<%= CSS_NAME8 %>.css': './markup/sass/<%= CSS_NAME8 %>.scss',
+                    '<%= PUBLIC_DIR %>/css/<%= CSS_NAME9 %>.css': './markup/sass/<%= CSS_NAME9 %>.scss',
                     '<%= PUBLIC_DIR %>/css/<%= CSS_STATIC_NAME %>.css': './markup/sass/<%= CSS_STATIC_NAME %>.scss'
-                    //'<%= PUBLIC_DIR %>/css/pc/editor_template.css': './develop/markup/sass/editor_template_pc.scss',
-                    //'<%= PUBLIC_DIR %>/css/mobile/editor_template.css': './develop/markup/sass/editor_template_mobile.scss',
-                    //'<%= PUBLIC_DIR %>/css/styles.css': './develop/markup/sass/styles.scss'
                 }
             }
         },
@@ -176,8 +118,6 @@ module.exports = function (grunt) {
                 src:
                 [
                     '<%= PUBLIC_DIR %>/css/**/*.css'
-                    // '!<%= PUBLIC_DIR %>/css/themes/*.css',
-                    // '!<%= PUBLIC_DIR %>/css/{bootstrap-tagsinput,plugins,styles}.css'
                 ]
             }
         },
@@ -211,6 +151,26 @@ module.exports = function (grunt) {
                     '<%= PUBLIC_DIR %>/css/<%= CSS_NAME4 %>.css': [
                         './markup/sass/common/_dummy_charset.scss',
                         '<%= PUBLIC_DIR %>/css/<%= CSS_NAME4 %>.css'
+                    ],
+                    '<%= PUBLIC_DIR %>/css/<%= CSS_NAME5 %>.css': [
+                        './markup/sass/common/_dummy_charset.scss',
+                        '<%= PUBLIC_DIR %>/css/<%= CSS_NAME5 %>.css'
+                    ],
+                    '<%= PUBLIC_DIR %>/css/<%= CSS_NAME6 %>.css': [
+                        './markup/sass/common/_dummy_charset.scss',
+                        '<%= PUBLIC_DIR %>/css/<%= CSS_NAME6 %>.css'
+                    ],
+                    '<%= PUBLIC_DIR %>/css/<%= CSS_NAME7 %>.css': [
+                        './markup/sass/common/_dummy_charset.scss',
+                        '<%= PUBLIC_DIR %>/css/<%= CSS_NAME7 %>.css'
+                    ],
+                    '<%= PUBLIC_DIR %>/css/<%= CSS_NAME8 %>.css': [
+                        './markup/sass/common/_dummy_charset.scss',
+                        '<%= PUBLIC_DIR %>/css/<%= CSS_NAME8 %>.css'
+                    ],
+                    '<%= PUBLIC_DIR %>/css/<%= CSS_NAME9 %>.css': [
+                        './markup/sass/common/_dummy_charset.scss',
+                        '<%= PUBLIC_DIR %>/css/<%= CSS_NAME9 %>.css'
                     ]
                 }
             }
@@ -223,7 +183,6 @@ module.exports = function (grunt) {
                     cwd: './markup/img',
                     src: [
                         '**/*',
-                        // '!**/spr_*'
                     ],
                     dest: 'markup/dist/assets/images'
                 }]
@@ -245,8 +204,6 @@ module.exports = function (grunt) {
                     cwd: 'markup/img',
                         src: [
                             '**/*.png'
-                            // '!**/spr_*.png',
-                            // '!**/spr_*/**/*.png'
                         ],
                         dest: 'markup/dist/assets/images'
                     }]
@@ -283,20 +240,6 @@ module.exports = function (grunt) {
                 files: ['./markup/sass/**/*.scss'],
                 tasks: ['build_sass', 'copy:static_resource', 'sync']
             }
-
-            // sprite: {
-            //     files: ['./markup/img/spr_*/**/*.png'],
-            //     tasks: ['sprite', 'build_sass', 'copy:static_resource', 'sync']
-            // },
-
-        //   image: {
-        //         files: [
-        //             'markup/img/**/*.png',
-        //             '!markup/img/spr_*.png',
-        //             '!markup/img/spr_*/**/*.png'
-        //         ],
-        //         tasks: ['sync']
-        //   }
         },
 
         concurrent: {
@@ -311,64 +254,11 @@ module.exports = function (grunt) {
     // default
     grunt.registerTask('default', ['clean', 'concurrent:build', 'watch']);
     grunt.registerTask('deploy', ['clean', 'sassNoMap', 'concurrent:deploy']);
-
     grunt.registerTask('build_sass', ['sass', 'postcss', 'cssmin']);
-    // grunt.registerTask('sass_init', ['sprite_common_func', 'sprite', 'build_sass', 'copy:static_resource']);
-    // grunt.registerTask('sass_init2', ['serverUrl', 'sprite_common_func', 'sprite', 'build_sass', 'copy:static_resource']);
     grunt.registerTask('sass_init', ['build_sass', 'copy:static_resource']);
-    // grunt.registerTask('sass_init2', ['build_sass', 'copy:static_resource']);
-
     grunt.registerTask('html', ['includereplace', 'markupfileindex']);
-    // grunt.registerTask('svgicon', ['svgmin', 'webfont', 'copy:fonts', 'sass:dev', 'postcss:dev', 'cssmin:dev', 'copy']);
-
-    // // sass task > cssTemplate, cssOpts 공통 function 추가
-    // grunt.registerTask('sprite_common_func', function () {
-    //     _.each(grunt.config.data.sprite, function(conf){
-    //         if(!!conf.cssTemplate === false){
-    //             conf.cssTemplate = function (params) {
-    //                 var template = grunt.file.read('./markup/sass/sprites/sprites.mustache');
-    //                 return Mustache.render(template, params);
-    //             }
-    //         }
-
-    //         if(!!conf.cssOpts === false){
-    //             conf.cssOpts = {
-    //                 removepx: function () {
-    //                     return function (text, render) {
-    //                         var value = render(text);
-    //                         return '0px' === value ? '0' : value;
-    //                     };
-    //                 },
-    //                 retina: function () {
-    //                     return function (text, render) {
-    //                         var pixelRatio = 2;
-    //                         return parseInt(render(text), 10) / pixelRatio + 'px';
-    //                     };
-    //                 }
-    //             }
-    //         }
-    //     });
-    // });
-
     // sass map 생성 여부
     grunt.registerTask('sassNoMap', function () {
         grunt.option('no-map', true);
     });
-
-    // grunt.registerTask('serverUrl', function () {
-    //     // var templateTaskName = ['spr_template', 'spr_template_2x'],
-    //     var url = '';
-
-    //     // if(e === 'deploy'){
-    //         url = '/biz/static/assets/images';
-    //         // grunt.config.data.IMG_DIR = 'https://static.hiddenx.co.kr/hidden/img';
-    //     //     url = '../../../../dist/assets/images';
-    //     // }
-
-    //     _.each(grunt.config.data.sprite, function(conf, key){
-    //         // if(templateTaskName.indexOf(key) !== -1) {
-    //         conf.imgPath = url + '/' + key + '.png';
-    //         //}
-    //     });
-    // });
 };
---
+++ Readme.md
@@ -0,0 +1,7 @@
+# 기본 html 환경
+전제 조건
+node, git, grunt-cli가 설치 되어있어야 합니다.
+
+1. clone을 받은 다음  npm install --> node_modules가 생성됩니다.
+2. grunt명령어만 치면 watch가 되어 실시간 변경되며, css는 map이 생성됩니다.
+3. grunt deploy 명령어를 치면 dist 폴더 안에 map이 없는 상태로 배포 가능하게 됩니다.
