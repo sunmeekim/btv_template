354e2b60f2981f90f73ae42986e25664ea88e21e
sunmee.kim <skt.sunmee.kim@sk.com>
2018-01-31 15:18:46 +0900

    AISTBQA-382 도움말포커스 오류 임시스타일 추가

--- markup/js/test_ui.js
+++ markup/js/test_ui.js
@@ -1,27 +1,31 @@
-$(function(){
-    // 쇼핑/주문 버튼 활성화
-    $('.order_btnbox .order_btnbox__button').on('click',function() {
-        $(this).addClass('active').siblings().removeClass('active');
-    });
-
-    // 카드 리스트 활성화
-    $('.card_wrap .card_wrap__item').on('click', function() {
-        if ( !$(this).hasClass('none') && !$(this).hasClass('active')) {
-            $(this).addClass('active').siblings().removeClass('active');
-        } else if ($(this).hasClass('active')) {
-            $(this).find('.iot_default_card__action').toggleClass('on');
-        }
-    });
-
-    // 프로야구 순위 목록 활성화
-    $('.teamrank_wrap__list .teamrank_wrap__list-item').on('click', function() {
-        $(this).addClass('active').siblings().removeClass('active').parent().siblings().find('.teamrank_wrap__list-item').removeClass('active');
-    });
-
-    $("#defaultOpen").click();
-
-});
+// $(function(){
+//     // 쇼핑/주문 버튼 활성화
+//     $('.order_btnbox .order_btnbox__button').on('click',function() {
+//         $(this).addClass('active').siblings().removeClass('active');
+//     });
+//
+//     // 카드 리스트 활성화
+//     $('.card_wrap .card_wrap__item').on('click', function() {
+//         if ( !$(this).hasClass('none') && !$(this).hasClass('active')) {
+//             $(this).addClass('active').siblings().removeClass('active');
+//         } else if ($(this).hasClass('active')) {
+//             $(this).find('.iot_default_card__action').toggleClass('on');
+//         }
+//     });
+//
+//     // 프로야구 순위 목록 활성화
+//     $('.teamrank_wrap__list .teamrank_wrap__list-item').on('click', function() {
+//         $(this).addClass('active').siblings().removeClass('active').parent().siblings().find('.teamrank_wrap__list-item').removeClass('active');
+//     });
+//
+//
+//
+// });
 // 인클루드페이지 스위칭 하기(jquery)
+
+$(function(){
+    $("#defaultOpen").click();
+});
 $('.case__link').on('click', function() {
     var displayType = $(this).attr('href');
     //console.log(displayType);
